9d009a0c8e0e6c51664ebe3c5ddc7663917f6388
qingnan <qingnan0192@gmail.com>
2023-11-13 19:25:15 +0800

    提交H5代码

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    
+  </div>
+</template>
+
+<script>
+ 
+
+export default {
+  name: 'App',
+  //components: {
+  //  HelloWorld
+  //},
+  methods:{
+    open(){
+      window.location.href = 'atrustGovDingtalk.html';
+    }
+  },
+  created () {
+   // this.open();
+  }
+
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
